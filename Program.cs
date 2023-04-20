c30ace4b364949963262600e13229795661c0c70
lumpia2 <cody.cacal@yahoo.com>
2023-04-20 11:45:16 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Application that previews keys
+
+using Microsoft.Toolkit.Uwp.Notifications;
+
+class commandPreview
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Provide key to display");
+        }
+
+        else
+        {
+            new ToastContentBuilder()
+                .AddText($"{args[0]}")
+                .Show();
+        }
+
+    }
+}
+
